638f68b7ca365ee92976490c607c85586a524f87
hoapham2k2 <20520995@gm.uit.edu.vn>
2024-07-03 13:31:06 +0700

    update backend production request

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 export const api = axios.create({
-	baseURL: `http://localhost:5036/api/v1`,
+	// baseURL: `http://localhost:5036/api/v1`,
+	baseURL: `${
+		process.env.NODE_ENV === "development"
+			? "http://localhost:5036"
+			: "https://ds-cms-backend.onrender.com" // Change this to your backend URL
+	}/api/v1`,
 	headers: {
 		"Content-Type": "application/json",
 	},
